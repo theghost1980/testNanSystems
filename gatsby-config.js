318bb2c2f8bb59779f470d6af7aa874e26f5600b
Saturno Mangieri <saturnob612@gmail.com>
2020-12-25 17:26:13 -0400

    adding gatsby-plugin-manifest + favicon 512x512 looking good :P

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,18 @@ module.exports = {
     author: `Saturno Mangieri -> saturnoman.com`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `NaNSYSTEMS.us`,
+        short_name: `NaNSYSTEMS.us`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/media-imgs/favIcon.png`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
