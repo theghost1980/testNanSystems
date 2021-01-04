59cae423b18ed26b75caeae84b5c0549f1ddf099
Saturno Mangieri <saturnob612@gmail.com>
2021-01-04 00:21:12 -0400

    adding the script of equalweb on gatsby-router, tested on local working, now testing on netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,12 +30,12 @@ const Index = (props) => {
     const dataVenture = (_lang === 'es' ? props.data.dataVenture_es : props.data.dataVenture_en);
     // console.log('dataBlogList',dataBlogList);
 
-    useEffect(() => {
-        // accesibility code
-        // <!-- Accessibility Code for "dreamy-keller-b0a352.netlify.app" -->
-        window.interdeal = { "sitekey": "570f10caadcf593e2b70c3ad9f7beb4d", "Position": "Left", "Menulang": "EN", "domains": { "js": "https://cdn.equalweb.com/", "acc": "https://access.equalweb.com/" }, "btnStyle": { "scale": [ "0.7", "0.7" ], "icon": { "type": 13, "shape": "circle", "outline": false }, "color": { "main": "#285790", "second": "" } } }; (function(doc, head, body){ var coreCall = doc.createElement('script'); coreCall.src = 'https://cdn.equalweb.com/core/2.1.8/accessibility.js'; coreCall.defer = true; coreCall.integrity = 'sha512-tA0/58RaxqQMY+p5wW7LgZM88ckav7DG0iT6VEUqGVyFvH6PcFkmMVuWQgqftDp3BYYHxjeYTAX14Ct7DS/fRQ=='; coreCall.crossOrigin = 'anonymous'; coreCall.setAttribute('data-cfasync', true ); body? body.appendChild(coreCall) : head.appendChild(coreCall); })(document, document.head, document.body);
-        // end test
-    });
+    // useEffect(() => {
+    //     // accesibility code
+    //     // <!-- Accessibility Code for "dreamy-keller-b0a352.netlify.app" -->
+    //     window.interdeal = { "sitekey": "570f10caadcf593e2b70c3ad9f7beb4d", "Position": "Left", "Menulang": "EN", "domains": { "js": "https://cdn.equalweb.com/", "acc": "https://access.equalweb.com/" }, "btnStyle": { "scale": [ "0.7", "0.7" ], "icon": { "type": 13, "shape": "circle", "outline": false }, "color": { "main": "#285790", "second": "" } } }; (function(doc, head, body){ var coreCall = doc.createElement('script'); coreCall.src = 'https://cdn.equalweb.com/core/2.1.8/accessibility.js'; coreCall.defer = true; coreCall.integrity = 'sha512-tA0/58RaxqQMY+p5wW7LgZM88ckav7DG0iT6VEUqGVyFvH6PcFkmMVuWQgqftDp3BYYHxjeYTAX14Ct7DS/fRQ=='; coreCall.crossOrigin = 'anonymous'; coreCall.setAttribute('data-cfasync', true ); body? body.appendChild(coreCall) : head.appendChild(coreCall); })(document, document.head, document.body);
+    //     // end test
+    // },[]);
 
     return (
         <Layout>
