94e5b519fa6fc4ead905893d4a5f5431bde17b09
Saturno Mangieri <saturnob612@gmail.com>
2021-01-13 12:24:33 -0400

    testing netlify forms

--- src/components/commentsForm.js
+++ src/components/commentsForm.js
@@ -3,26 +3,26 @@ import React from 'react';
 const Commentsform = () => {
     return (
         <div className="commentsFormCont">
-            <form className="commentsForm" name="comments" method="POST" data-netlify="true">
+            <form className="commentsForm" name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
                 <h2 className="centeredTitle titlePost">
                     Have any comments?
                 </h2>
-                <p>
-                    <label>Your Name: <input type="text" name="name" /></label>   
+                <p className="formPComments">
+                    <label className="formPComments">Your Name: <input type="text" name="name" /></label>   
                 </p>
-                <p>
+                <p className="formPComments">
                     <label>Your Email: <input type="email" name="email" /></label>
                 </p>
-                <p>
+                <p className="formPComments">
                     <label>Your Role: <select name="role[]" multiple>
                     <option value="leader">Leader</option>
                     <option value="follower">Follower</option>
                     </select></label>
                 </p>
-                <p>
+                <p className="formPComments">
                     <label>Message: <textarea name="message"></textarea></label>
                 </p>
-                <p>
+                <p className="formPComments">
                     <button type="submit">Send</button>
                 </p>
                 </form>
